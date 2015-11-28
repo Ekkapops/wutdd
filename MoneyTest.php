d48d73740e6965c20cfa6710ef7daa14a0cdaf36
Ekkapop Suwanna <vksettawut@gmail.com>
2015-11-28 14:05:07 +0700

    testPHPUnit

--- MoneyTest.php
+++ MoneyTest.php
@@ -15,5 +15,25 @@ class MoneyTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(-1, $b->getAmount());
     }
 
+    public function testCanBeNegated2()
+    {
+        // Arrange
+        $a = new Money(1);
+
+        // Act
+        $b = $a->negate();
+
+        // Assert
+        $this->assertEquals(1, $b->getAmount());
+    }
+
+    public function testPlus()
+    {
+        // Arrange
+        $a = 1+2;
+        // Assert
+        $this->assertEquals(4, $a);
+    }
+
     // ...
 }
